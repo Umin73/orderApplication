40623b7058ebff32d90b6e1725aec8c8947fb496
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-05-01 22:19:05 +0900

    Feat: Cart API 추가

---
+++ src/main/java/com/study/orderApplication/entity/Cart.java
@@ -0,0 +1,27 @@
+package com.study.orderApplication.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private Item item;
+
+    private Integer quantity;
+
+    private LocalDateTime addedDateTime = LocalDateTime.now();
+}
