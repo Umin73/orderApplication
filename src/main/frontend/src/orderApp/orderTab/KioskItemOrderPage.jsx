ad3ee54a99f43491bcc25df32357156120fb7447
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-05-01 17:38:27 +0900

    Feat: 상품 주문 추가

--- src/main/frontend/src/orderApp/orderTab/KioskItemOrderPage.jsx
+++ src/main/frontend/src/orderApp/orderTab/KioskItemOrderPage.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import KioskNavBar from "../KioskNavBar";
 import KioskHeader from "../KioskHeader";
+import KioskItemOrder from "./KioskItemOrder";
+import {useLocation} from "react-router-dom";
 
 function KioskItemOrderPage() {
+
+    const location = useLocation();
+    const {item} = location.state;
 
     return (
         <>
             <KioskHeader isNextPage={true} pageName=""/>
             <div className="kiosk-main-container" style={{padding: "15px", paddingTop: '70px'}}>
-
+                <KioskItemOrder item={item} />
             </div>
             <KioskNavBar/>
         </>
--- src/main/frontend/src/orderApp/orderTab/KioskOrderList.jsx
+++ src/main/frontend/src/orderApp/orderTab/KioskOrderList.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import "./KioskOrderList.css";
 
 export default function KioskOrderList() {
+
+    const navigate = useNavigate();
 
     const categoryList = ["커피", "티", "블렌디드", "프라푸치노", "기타"];
     const [selectedCategory, setSelectedCategory] = useState("커피");
@@ -38,7 +41,8 @@ export default function KioskOrderList() {
 
                 <div className="item-grid">
                     {itemList.map((item, idx) => (
-                        <div className="item-card" key={idx}>
+                        <div className="item-card" key={idx}
+                             onClick={() => navigate("/kiosk/order-item", { state: { item } })}>
                             <img src={item.itemImageUrl} alt={item.itemName} className="item-image"/>
                             <div className="item-name">{item.itemName}</div>
                             <div className="item-price">{item.itemPrice.toLocaleString()}원</div>
