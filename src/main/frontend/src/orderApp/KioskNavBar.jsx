d2c393627a6b0011ff19ab842a21de4f8df81ad6
SoSo12121212 <yo789yo25@gmail.com>
2025-05-03 16:19:12 +0900

    feat: 키오스크 내비바 아이콘 추가

--- src/main/frontend/src/orderApp/KioskNavBar.jsx
+++ src/main/frontend/src/orderApp/KioskNavBar.jsx
@@ -2,20 +2,62 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import "./KioskNavBar.css"
 
+const containerStyle={
+    width:"100%",
+    height:"60px",
+  //margin:"10px",
+   display:"flex",
+    //flexDirection:"column",
+   //alignItems:"center",
+   padding:"0 5px",
+   boxSizing:"border-box",
 
-export default function KioskNavBar() {
+};
+
+const itemContainerStyle={
+display:"flex",
+alignItems:"center",
+justifyContent:"space-around", //균등 간격 배분
+width:"100%",
+height:"100%",
+};
+
+
+//아이콘과 텍스트를 세로로 배치
+const linkitemStyle={
+    display:"flex",
+    flexDirection:"column",
+    alignItems:"center",
+    textDecoration:"none",
+    color:"#333",
+};
+
+
+
+    export default function KioskNavBar() {
 
     return (
-        <div className="kiosk-nav-bar-container">
-            <div className="kiosk-nav-bar-items">
-                <Link className="kiosk-nav-bar-item" to="/kiosk/main" style={{ textDecoration: "none", color: "inherit" }}>
-                    HOME
+        <div className="kiosk-nav-bar-container" style={containerStyle}>
+            <div className="kiosk-nav-bar-items" style={itemContainerStyle}>
+
+                <Link className="kiosk-nav-bar-item" to="/kiosk/main" style={linkitemStyle}>
+                    <img src={process.env.PUBLIC_URL + '/home.png'} alt="홈아이콘" />
+                    <span>Home</span>
+                    </Link>
+
+                <Link className="kiosk-nav-bar-item" to="/kiosk/order" style={linkitemStyle}>
+                    <img src={process.env.PUBLIC_URL + '/PAY.png'} alt="오더아이콘" />
+                    <span>PAY</span>
                 </Link>
-                <Link className="kiosk-nav-bar-item" to="/kiosk/order" style={{ textDecoration: "none", color: "inherit" }}>
-                    ORDER
+
+                <Link className="kiosk-nav-bar-item" to="/kiosk/order" style={linkitemStyle}>
+                    <img src={process.env.PUBLIC_URL + '/Order.png'} alt="오더아이콘"  />
+                    <span>ORDER</span>
                 </Link>
-                <Link className="kiosk-nav-bar-item" to="/signup" style={{ textDecoration: "none", color: "inherit" }}>
-                    MY
+
+                <Link className="kiosk-nav-bar-item" to="/signup" style={linkitemStyle}>
+                    <img src={process.env.PUBLIC_URL + '/user.png'} alt="마이페이지아이콘"   />
+                    <span>MY</span>
                 </Link>
             </div>
         </div>
